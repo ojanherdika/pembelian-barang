ac117625d44f71b60b623cff798532072512075f
Fauzan Herdika Tubagus Putra <ojanherdika@gmail.com>
2024-01-23 11:55:51 +0700

    view login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,21 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h2>Login</h2>
-
-    <form method="POST" action="{{ route('sign-in') }}">
-        @csrf
-
-        <label for="email">Email:</label>
-        <input type="email" name="email" required>
-
-        <label for="password">Password:</label>
-        <input type="password" name="password" required>
-
-        <button type="submit">Login</button>
-    </form>
+    <div class="container"><br>
+        <div class="col-md-4 col-md-offset-4">
+            <h2 class="text-center">Aplikasi Barang</h3>
+            <hr>
+            @if(session('error'))
+            <div class="alert alert-danger">
+                <b>Opps!</b> {{session('error')}}
+            </div>
+            @endif
+            <form action="{{ route('sign-in') }}" method="post">
+            @csrf
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control" placeholder="Password" required="">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Log In</button>
+                <hr>
+                <p class="text-center">Belum punya akun? <a href="#">Register</a> sekarang!</p>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
